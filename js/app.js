51c4137e1377360dcd1b096ecc62485e76afaa56
TheKailaCode <kailakelly0@gmail.com>
2022-09-24 03:40:53 -0400

    debugged event listener to add button

--- js/app.js
+++ js/app.js
@@ -1,21 +1,18 @@
-//document.getElementById('pic-button').addEventListener("click", getPicture);
+document.getElementById('pic-button').addEventListener("click", getPicture);
 
-// function getPicture() {
+async function getPicture() {
+    await fetch('https://api.nasa.gov/planetary/apod?api_key=SkaBAkfRcBrstYk0upNqvyf9vbF6Yr324SHazddO')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            document.querySelector('img').src = data.url
+            document.querySelector('span').innerText = data.date
+            document.querySelector('h2').innerText = data.title
+            document.querySelector('p').innerText = data.explanation
+        })
+}
 
 
-
-//     // document.getElementsByClassName('nasa-img').source = data.hdurl
-// }
-
-fetch('https://api.nasa.gov/planetary/apod?api_key=SkaBAkfRcBrstYk0upNqvyf9vbF6Yr324SHazddO')
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        document.querySelector('img').src = data.url
-        document.querySelector('span').innerText = data.date
-        document.querySelector('h2').innerText = data.title
-        document.querySelector('p').innerText = data.explanation
-    })
 
 
 
